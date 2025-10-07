f23d9985b4dde83801993e05b0dc038b37804eb0
bimanyuw <f.abimanyuwijanarko@gmail.com>
2025-10-07 11:39:16 +0700

    update

--- footballshop/settings.py
+++ footballshop/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / 'staticfiles'
