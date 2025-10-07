f408a459b2da76ef9dbcab5d62820f119a0e873e
bimanyuw <f.abimanyuwijanarko@gmail.com>
2025-10-08 06:40:07 +0700

    update

--- footballshop/settings.py
+++ footballshop/settings.py
@@ -9,16 +9,13 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
-from pathlib import Path
-
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 # Load environment variables from .env file
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env") 
 
 
 # Quick-start development settings - unsuitable for production
